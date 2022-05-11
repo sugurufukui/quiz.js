7002fb6ca5b1ca68025529f36e20f785d413bcb4
sugurufukui <2ar.1225@gmail.com>
2022-05-12 05:19:59 +0900

    完成

---
+++ index.js
@@ -0,0 +1,65 @@
+const quiz =[
+  {
+    question: 'ゲーム市場、最も売れたゲーム機はどれ？',
+    answers: ['スーパーファミコン', 'プレイステーション2', 'ニンテンドースイッチ', 'ニンテンドーDS'],
+    correct: 'ニンテンドーDS'
+  }, {
+    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    answers: ['MOTHER2', 'スーパーマリオブラザーズ3', 'スーパードンキーコング', '星のカービィ'],
+    correct: 'MOTHER2'
+  }, {
+    question: 'ファイナルファンタジーⅣの主人公の名前は？',
+    answers: ['フリオニール', 'クラウド', 'ティーダ', 'セシル'],
+    correct: 'セシル'
+  }
+];
+
+let quizIndex = 0;
+const quizLength =quiz.length;
+let score = 0;
+
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  let buttonIndex = 0;
+  while(buttonIndex < buttonLength){
+  $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex]
+  buttonIndex ++;
+  }
+}
+
+setupQuiz();
+
+//クリックされたら作動開始
+const crickHandler = (e) => {
+  if(quiz[quizIndex].correct === e.target.textContent){
+    window.alert('正解！！')
+    score++;
+  } else {
+    window.alert('不正解！！')
+  }
+
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+    //問題数がまだあればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数がもうなければこちらを実行
+    window.alert('終了！　あなたの正解数は' + score +' / '+ quizLength +'でした')
+  }
+};
+
+//ボタンをクリックした正誤判定
+let handlerIndex = 0;
+
+while (handlerIndex < buttonLength) {
+  //ここに命令を記載
+  $button[handlerIndex].addEventListener('click', (e)=> {
+    crickHandler(e);
+  });
+  handlerIndex++;
+}
